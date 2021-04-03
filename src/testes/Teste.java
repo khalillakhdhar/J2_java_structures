c6a8ee03a3bcdc33c0d366b5a5afb06e3d5b814a
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-03 13:22:36 +0100

    Update Teste.java

--- src/testes/Teste.java
+++ src/testes/Teste.java
@@ -1,10 +1,13 @@
 package testes;
 
+import classes.Indice;
+
 public class Teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Indice i=new Indice("Ahmed Ahmed", 87, (float) 1.83);
+System.out.println(i.toString());
 	}
 
 }
