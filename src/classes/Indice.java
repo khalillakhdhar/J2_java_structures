0996ab5a21de2aab1c74332a1b749624367960e5
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-03 13:12:09 +0100

    Update Indice.java

--- src/classes/Indice.java
+++ src/classes/Indice.java
@@ -1,34 +1,48 @@
 package classes;
 
 public class Indice {
-	 private String nom; 
-	private float poid,taille;
+	private String nom;
+	private float poid, taille;
+
 	public String getNom() {
 		return nom;
 	}
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 	public float getPoid() {
 		return poid;
 	}
+
 	public void setPoid(float poid) {
 		this.poid = poid;
 	}
+
 	public float getTaille() {
 		return taille;
 	}
+
 	public void setTaille(float taille) {
 		this.taille = taille;
 	}
-	
-	
-	public int calcule()
-	{
-		double imc= poid/(Math.pow(taille, 2));
-		return Math.round(imc);
-		
+
+	public int calcule() {
+		double imc = poid / (Math.pow(taille, 2));
+		return (int) Math.round(imc);
+
 	}
-	
+
+	public String evaluer() {
+		int masse = calcule();
+		if (masse < 20) {
+			return "maigre";
+		} else if (masse <= 25)
+			return "idéale";
+		else
+			return "surpoid";
+
+	}
 
 }
